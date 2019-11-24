aeb61fa7f78636c40c3439e510b9bd4ff1f951e7
github username <github email address>
2019-11-24 10:57:14 -0700

    commit this new issue

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,4 @@ fido = Dog.new
 snoopy = Dog.new 
 lassie = Dog.new
 
-#This lab was completed earlier, needing to push it back up onto github and learn.co
+#This lab was completed earlier, needing to push it back up onto github and learn.co, issues with connecting to learn.co.

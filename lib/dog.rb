6a614ca233b419127c58af6630a9265fd12f7de2
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-24 17:59:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,4 @@ fido = Dog.new
 snoopy = Dog.new 
 lassie = Dog.new
 
-<<<<<<< HEAD
 #This lab was completed earlier, needing to push it back up onto github and learn.co, issues with connecting to learn.co.
-=======
->>>>>>> 47dff93834d22f360a45ed2935881c6c0f39c2b5
